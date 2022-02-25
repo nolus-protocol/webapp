5ad3651ca0d9c86049d9755990761559e2a0b272
Rusi Boyadjiev <rusiboyadjiev@gmail.com>
2022-02-25 12:19:30 +0200

    Initial Wallet commit
    Signed-off-by: Rusi Boyadjiev <rusiboyadjiev@gmail.com>

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="sass">
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TestComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TestComponent from '@/components/TestComponent.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    TestComponent
+  }
+})
+</script>
