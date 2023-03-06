6f9cdd4e3474a633b41a77d91ab7d674e833af58
Alexander Makedonski <alexanderm@prime-code.com>
2023-03-06 09:16:47 +0200

    fix: osmosis channel

--- src/networks/config.ts
+++ src/networks/config.ts
@@ -1,5 +1,5 @@
-import type { NetworkData } from "@/types/Network";
 import osmoIcon from "@/assets/icons/coins/osmosis.svg";
+import type { NetworkData } from "@/types/Network";
 import { NATIVE_NETWORK } from "@/config/env";
 import { NETWORK as OSMO_NETWORK } from './osmo/network';
 
@@ -26,22 +26,46 @@ export const NETWORKS_DATA: {
 } = {
     localnet: {
         supportedNetworks: {
-            OSMO: OSMO_NETWORK
+            OSMO: {
+                ...OSMO_NETWORK,
+                tendermintRpc: "https://rpc-test.osmosis.zone:443",
+                api: "https://lcd-test.osmosis.zone",
+                sourceChannel: "channel-2406",
+                explorer: 'https://testnet.mintscan.io/osmosis-testnet/txs',
+            }
         }
     },
     devnet: {
         supportedNetworks: {
-            OSMO: OSMO_NETWORK
+            OSMO: {
+                ...OSMO_NETWORK,
+                tendermintRpc: "https://rpc-test.osmosis.zone:443",
+                api: "https://lcd-test.osmosis.zone",
+                sourceChannel: "channel-2406",
+                explorer: 'https://testnet.mintscan.io/osmosis-testnet/txs',
+            }
         }
     },
     testnet: {
         supportedNetworks: {
-            OSMO: OSMO_NETWORK
+            OSMO: {
+                ...OSMO_NETWORK,
+                tendermintRpc: "https://rpc-test.osmosis.zone:443",
+                api: "https://lcd-test.osmosis.zone",
+                sourceChannel: "channel-1837",
+                explorer: 'https://testnet.mintscan.io/osmosis-testnet/txs',
+            }
         }
     },
     mainnet: {
         supportedNetworks: {
-            OSMO: OSMO_NETWORK
+            OSMO: {
+                ...OSMO_NETWORK,
+                tendermintRpc: "https://rpc-test.osmosis.zone:443",
+                api: "https://lcd-test.osmosis.zone",
+                sourceChannel: "channel-1837",
+                explorer: 'https://testnet.mintscan.io/osmosis-testnet/txs',
+            }
         }
     },
 };
