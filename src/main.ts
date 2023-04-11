7a2afa7f34baf24349cbe800a48c5e146b7994e8
Alexander <alexanderm@prime-code.com>
2023-04-11 19:37:27 +0300

    update: lease stroke button

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,10 @@ import "@/assets/styles/global.scss";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createI18n } from "vue-i18n";
 
 import App from "@/App.vue";
 import router from "@/router";
-import en from "@/locales/en";
-
-export const i18n = createI18n({
-    legacy: false,
-    locale: "en",
-    fallbackLocale: "en",
-    messages: { en }
-});
+import { i18n } from "./i18n";
 
 const app = createApp(App);
 app.use(router);
