a2e03f62aa801c1aed651fd0324a8c7b6c6648d8
Alexander <alexanderm@prime-code.com>
2023-05-23 14:44:30 +0300

    change dev mode

--- src/utils/AppUtils.ts
+++ src/utils/AppUtils.ts
@@ -1,7 +1,11 @@
+enum Mode {
+    dev = 'dev',
+    prod = 'prod'
+}
 export class ApptUtils {
 
     static isDev(){
-        return import.meta.env.DEV;
+        return import.meta.env.VITE_MODE == Mode.dev;
     }
 
 }
