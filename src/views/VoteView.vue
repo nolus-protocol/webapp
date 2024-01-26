94015261b6da31eed3131c4071d65ee5814faa3c
Teodor Dobrev <teodor.working@gmail.com>
2024-01-26 11:29:16 +0200

    code improvements

--- src/views/VoteView.vue
+++ src/views/VoteView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="{ 'animate-pulse': !initialLoad }" class="block">
-    <template v-if="initialLoad && !showSkeleton">
+  <div :class="{ 'animate-pulse': !state.initialLoad }" class="block">
+    <template v-if="state.initialLoad && !state.showSkeleton">
       <TransitionGroup
         appear
         class="flex flex-wrap flex-row lg:gap-x-5 gap-y-8"
@@ -8,7 +8,7 @@
         tag="div"
       >
         <ProposalItem
-          v-for="(proposal, index) in proposals"
+          v-for="(proposal, index) in state.proposals"
           :key="index"
           :state="proposal"
           @vote="onVote"
@@ -18,27 +18,30 @@
       <div class="text-center mt-6 lg:mt-8">
         <button
           v-if="visible"
-          :class="{ 'js-loading': loading }"
+          :class="{ 'js-loading': state.loading }"
           class="btn btn-secondary btn-medium-secondary mx-auto"
           @click="loadMoreProposals"
         >
           {{ $t('message.load-more') }}
         </button>
       </div>
-      <Modal v-if="showReadMoreModal" @close-modal="onCloseReadMoreModal">
-        <ProposalReadMoreDialog :source="proposal.description" :title="proposal.title" />
+      <Modal v-if="state.showReadMoreModal" @close-modal="onCloseReadMoreModal">
+        <ProposalReadMoreDialog
+          :source="state.proposal.description"
+          :title="state.proposal.title"
+        />
       </Modal>
-      <Modal v-if="showVoteModal" @close-modal="onCloseVoteModal">
-        <ProposalVoteDialog :proposal="proposal" />
+      <Modal v-if="state.showVoteModal" @close-modal="onCloseVoteModal">
+        <ProposalVoteDialog :proposal="state.proposal" />
       </Modal>
     </template>
     <template v-else>
       <ProposalSkeleton />
     </template>
   </div>
-  <Modal v-if="showErrorDialog" route="alert" @close-modal="showErrorDialog = false">
+  <Modal v-if="state.showErrorDialog" route="alert" @close-modal="state.showErrorDialog = false">
     <ErrorDialog
-      :message="errorMessage"
+      :message="state.errorMessage"
       :title="$t('message.error-connecting')"
       :try-button="onClickTryAgain"
     />
@@ -46,7 +49,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, onUnmounted, ref } from 'vue'
+import { computed, onMounted, onUnmounted, reactive } from 'vue'
 import { AppUtils } from '@/utils/AppUtils'
 import { type Proposal } from '@/modules/vote/Proposal'
 import ProposalItem from '@/modules/vote/components/ProposalItem.vue'
@@ -56,110 +59,139 @@ import Modal from '@/components/modals/templates/Modal.vue'
 import ProposalSkeleton from '@/modules/vote/components/ProposalSkeleton.vue'
 import ErrorDialog from '@/components/modals/ErrorDialog.vue'
 
-const showErrorDialog = ref(false)
-const errorMessage = ref('')
-const showReadMoreModal = ref(false)
-const showVoteModal = ref(false)
-const loading = ref(false)
-const initialLoad = ref(false)
-const showSkeleton = ref(true)
+interface CustomError extends Error {
+  message: string
+}
 
-const proposals = ref([] as Proposal[])
-const proposal = ref({
-  id: '',
-  title: '',
-  description: ''
+const LOAD_TIMEOUT = 500
+
+// const showErrorDialog = ref(false)
+// const errorMessage = ref('')
+// const showReadMoreModal = ref(false)
+// const showVoteModal = ref(false)
+// const loading = ref(false)
+// const initialLoad = ref(false)
+// const showSkeleton = ref(true)
+//
+// const proposals = ref([] as Proposal[])
+// const proposal = ref({
+//   id: '',
+//   title: '',
+//   description: ''
+// })
+// const limit = ref(6)
+// const pagination = ref({
+//   total: 0,
+//   next_key: ''
+// })
+// let timeout: NodeJS.Timeout
+
+const state = reactive({
+  showErrorDialog: false,
+  errorMessage: '',
+  showReadMoreModal: false,
+  showVoteModal: false,
+  loading: false,
+  initialLoad: false,
+  showSkeleton: true,
+  proposals: [] as Proposal[],
+  proposal: {
+    id: '',
+    title: '',
+    description: ''
+  },
+  limit: 6,
+  pagination: {
+    total: 0,
+    next_key: ''
+  },
+  timeout: null as NodeJS.Timeout | null
 })
-const limit = ref(6)
-const pagination = ref({
-  total: 0,
-  next_key: ''
-})
-let timeout: NodeJS.Timeout
 
 onMounted(async () => {
   await fetchGovernanceProposals()
 })
 
 onUnmounted(() => {
-  if (timeout) {
-    clearTimeout(timeout)
+  if (state.timeout) {
+    clearTimeout(state.timeout)
   }
 })
 
-const fetchGovernanceProposals = async () => {
+const fetchData = async (url: string) => {
   try {
-    const node = await AppUtils.getArchiveNodes()
-    const req = await fetch(
-      `${node.archive_node_api}/cosmos/gov/v1beta1/proposals?pagination.limit=${limit.value}&pagination.reverse=true&pagination.countTotal=true`
-    )
-
-    const data = await req.json()
-
-    proposals.value = data.proposals
-    pagination.value = data.pagination
-
-    initialLoad.value = true
-  } catch (error: Error | any) {
-    showErrorDialog.value = true
-    errorMessage.value = error?.message
-  } finally {
-    timeout = setTimeout(() => {
-      showSkeleton.value = false
-    }, 400)
+    const req = await fetch(url)
+    return await req.json()
+  } catch (error: CustomError | any) {
+    state.showErrorDialog = true
+    state.errorMessage = error.message
+    console.error(error)
+    return null
   }
 }
 
+const fetchGovernanceProposals = async () => {
+  const node = await AppUtils.getArchiveNodes()
+  const data = await fetchData(
+    `${node.archive_node_api}/cosmos/gov/v1beta1/proposals?pagination.limit=${state.limit}&pagination.reverse=true&pagination.countTotal=true`
+  )
+
+  if (!data) return
+
+  state.proposals = data.proposals
+  state.pagination = data.pagination
+
+  state.initialLoad = true
+  state.timeout = setTimeout(() => {
+    state.showSkeleton = false
+  }, LOAD_TIMEOUT)
+}
+
 const onReadMore = ({ description, title }: { description: string; title: string }) => {
-  showReadMoreModal.value = true
-  proposal.value = {
-    ...proposal.value,
+  state.showReadMoreModal = true
+  state.proposal = {
+    ...state.proposal,
     description,
     title
   }
 }
 
 const onCloseReadMoreModal = () => {
-  showReadMoreModal.value = false
+  state.showReadMoreModal = false
 }
 
 const onVote = (selectedProposal: Proposal) => {
-  showVoteModal.value = true
-  proposal.value = {
-    ...proposal.value,
+  state.showVoteModal = true
+  state.proposal = {
+    ...state.proposal,
     title: selectedProposal.content.title,
     id: selectedProposal.proposal_id
   }
 }
 
 const onCloseVoteModal = () => {
-  showVoteModal.value = false
+  state.showVoteModal = false
 }
 
 const visible = computed(() => {
-  return initialLoad.value && pagination.value.next_key
+  return state.initialLoad && state.pagination.next_key
 })
 
 const loadMoreProposals = async () => {
-  try {
-    loading.value = true
-    const node = await AppUtils.getArchiveNodes()
-    const req = await fetch(
-      `${node.archive_node_api}/cosmos/gov/v1beta1/proposals?pagination.limit=${limit.value}&pagination.key=${pagination.value.next_key}&pagination.reverse=true&pagination.countTotal=true`
-    )
+  state.loading = true
+  const node = await AppUtils.getArchiveNodes()
+  const data = await fetchData(
+    `${node.archive_node_api}/cosmos/gov/v1beta1/proposals?pagination.limit=${state.limit}&pagination.key=${state.pagination.next_key}&pagination.reverse=true&pagination.countTotal=true`
+  )
 
-    const data = await req.json()
+  if (!data) return
 
-    proposals.value = [...proposals.value, ...data.proposals]
-    pagination.value = data.pagination
-  } catch (error: Error | any) {
-    showErrorDialog.value = true
-    errorMessage.value = error?.message
-  } finally {
-    setTimeout(() => {
-      loading.value = false
-    }, 500)
-  }
+  state.proposals = [...state.proposals, ...data.proposals]
+  state.pagination = data.pagination
+
+  setTimeout(() => {
+    state.loading = false
+  }, LOAD_TIMEOUT)
 }
 
 async function onClickTryAgain() {
