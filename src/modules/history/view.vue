27d85e348b8bddbaaae07d424dc8e4ca6696cdeb
Teodor Dobrev <teodor.working@gmail.com>
2024-04-23 17:10:32 +0300

    add tailwind base configuration, migrate history page to use web-components library

--- src/modules/history/view.vue
+++ src/modules/history/view.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mb-sm-nolus-70p col-span-12">
     <!-- Header -->
-    <div class="table-header mt-[25px] flex flex-wrap items-center items-baseline justify-between px-4 lg:px-0">
+    <div class="table-header mt-[25px] flex flex-wrap items-baseline justify-between px-4 lg:px-0">
       <div class="left">
         <h1 class="nls-font-700 m-0 text-20 text-primary">
           {{ $t("message.history") }}
@@ -9,100 +9,75 @@
       </div>
     </div>
     <!-- History -->
-    <div
-      class="background shadow-box async-loader mt-6 block overflow-hidden lg:rounded-xl"
+    <Table
       :class="{ outline: hasOutline }"
+      :columns="columns"
+      class="async-loader mt-6"
+      columnsClasses="hidden md:flex"
     >
-      <!-- Assets -->
-      <div class="block p-4 lg:p-6">
-        <HistoryTableHeader />
-        <div
-          class="history-items-container block"
-          :class="{ 'animate-pulse': !initialLoad }"
-        >
-          <template v-if="initialLoad && !showSkeleton">
-            <TransitionGroup
-              name="fade-long"
-              appear
-            >
-              <HistoryTableItem
-                v-for="transaction of transactions"
-                :key="transaction.id"
-                :transaction="transaction"
-              />
-              <div
-                v-if="transactions.length == 0"
-                class="h-[180px]"
-              >
-                <div class="nls-12 text-dark-grey flex h-full flex-col items-center justify-center">
-                  <img
-                    src="/src/assets/icons/empty_history.svg"
-                    class="m-4 inline-block"
-                    height="32"
-                    width="32"
-                  />
-                  {{ $t("message.no-results") }}
-                </div>
-              </div>
-            </TransitionGroup>
-          </template>
-          <template v-else>
-            <div
-              v-for="index in 10"
-              :key="index"
-              class="asset-partial nolus-box border-standart relative flex h-[82px] flex-col items-stretch justify-between border-b px-4 py-3 md:h-[53px] md:flex-row md:items-center"
-            >
-              <div class="flex flex-1 md:flex-[9]">
-                <div class="flex max-w-[60%] flex-col justify-between md:flex-row">
-                  <div class="h-1.5 w-[9rem] rounded-full bg-grey"></div>
-                  <div class="h-1.5 w-[15rem] rounded-full bg-grey md:ml-10"></div>
-                </div>
-              </div>
-              <div class="flex flex-1 items-center justify-between md:flex-[3]">
-                <div class="h-1.5 w-24 rounded-full bg-grey md:flex"></div>
-                <div class="h-1.5 w-24 rounded-full bg-grey md:flex"></div>
-              </div>
-            </div>
-          </template>
-        </div>
-      </div>
-    </div>
+      <template v-slot:body>
+        <template v-if="initialLoad && !showSkeleton">
+          <TransitionGroup
+            appear
+            name="fade-long"
+          >
+            <HistoryTableRowWrapper
+              v-for="transaction of transactions"
+              :key="transaction.id"
+              :transaction="transaction"
+            />
+          </TransitionGroup>
+        </template>
+        <template v-else>
+          <HistoryTableSkeleton />
+        </template>
+      </template>
+    </Table>
     <div class="my-4 flex justify-center">
-      <button
+      <Button
         v-if="visible"
-        class="btn btn-secondary btn-medium-secondary mx-auto"
-        :class="{ 'js-loading': loading }"
+        :label="$t('message.load-more')"
+        :loading="loading"
+        class="mx-auto"
+        severity="secondary"
+        size="medium"
         @click="load"
-      >
-        {{ $t("message.load-more") }}
-      </button>
+      />
     </div>
   </div>
   <Modal
     v-if="showErrorDialog"
-    @close-modal="showErrorDialog = false"
     route="alert"
+    @close-modal="showErrorDialog = false"
   >
     <ErrorDialog
-      :title="$t('message.error-connecting')"
       :message="errorMessage"
+      :title="$t('message.error-connecting')"
       :try-button="onClickTryAgain"
     />
   </Modal>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import Modal from "@/common/components/modals/templates/Modal.vue";
 import ErrorDialog from "@/common/components/modals/ErrorDialog.vue";
 
 import { type ITransaction } from "./types";
-import { onMounted, onUnmounted, ref, computed } from "vue";
-import { HistoryTableHeader, HistoryTableItem } from "./components";
+import { computed, onMounted, onUnmounted, ref } from "vue";
 import { NetworkUtils } from "@/common/utils";
+import { Button, Table } from "web-components";
+import HistoryTableRowWrapper from "@/modules/history/components/HistoryTableRowWrapper.vue";
+import HistoryTableSkeleton from "@/modules/history/components/HistoryTableSkeleton.vue";
 
 const showErrorDialog = ref(false);
 const errorMessage = ref("");
 const transactions = ref([] as ITransaction[]);
+const columns = [
+  { label: "Tx hash", class: "max-w-[200px]" },
+  { label: "Action" },
+  { label: "Fee", class: "max-w-[200px]" },
+  { label: "Time", class: "max-w-[200px]" }
+];
 
 const senderPerPage = 10;
 let senderPage = 1;
@@ -192,7 +167,7 @@ async function load() {
   } finally {
     setTimeout(() => {
       loading.value = false;
-    }, 500);
+    }, 200);
   }
 }
 
