c6262083a242a1f79346db9d2a955eeaa39337b8
Teodor Dobrev <teodor.working@gmail.com>
2024-05-19 17:44:23 +0300

    finish refactoring earn page

--- src/modules/earn/components/index.ts
+++ src/modules/earn/components/index.ts
@@ -1,5 +1,6 @@
 import EarnLpnAsset from "./EarnLpnAsset.vue";
 import EarnNativeAsset from "./EarnNativeAsset.vue";
 import EarnReward from "./EarnReward.vue";
+import EarnAssetSkeleton from "./EarnAssetSkeleton.vue";
 
-export { EarnLpnAsset, EarnNativeAsset, EarnReward };
+export { EarnLpnAsset, EarnNativeAsset, EarnReward, EarnAssetSkeleton };
--- src/modules/earn/view.vue
+++ src/modules/earn/view.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mb-sm-nolus-70 col-span-12">
+  <div class="col-span-12">
     <!-- Header -->
     <div class="flex flex-wrap items-center justify-between px-4 lg:px-0 lg:pt-[25px]">
       <div class="left w-full md:w-1/2">
-        <h1 class="nls-font-700 nls-sm-title m-0 text-20 text-primary">
+        <h1 class="m-0 text-20 font-semibold text-neutral-typography-200">
           {{ $t("message.earn-title") }}
         </h1>
       </div>
@@ -11,114 +11,18 @@
 
     <div class="md:grid md:grid-cols-12 md:gap-4">
       <div class="md:col-span-7 lg:col-span-7">
-        <!-- Portfolio -->
-        <div
-          class="background async-loader border-standart shadow-box order-1 order-2 block p-5 outline md:col-span-7 md:mt-6 md:rounded-xl"
-        >
-          <div class="block items-center justify-between lg:flex">
-            <h2 class="nls-font-500 my-0 text-left text-16 text-primary">
-              {{ $t("message.earning-assets") }}
-            </h2>
-          </div>
-          <!-- Assets -->
-          <div class="mt-6 block md:mt-[25px]">
-            <!-- Assets Header -->
-
-            <div class="border-standart grid grid-cols-2 gap-6 border-b pb-3 md:grid-cols-3">
-              <div class="nls-font-500 text-dark-grey text-upper text-left text-12">
-                {{ $t("message.asset") }}
-              </div>
-
-              <div
-                class="nls-font-500 text-dark-grey text-upper md:m-r[10px] inline-flex hidden items-center justify-end text-center text-12 md:col-span-1 md:flex"
-              >
-                <span class="inline-block">{{ $t("message.deposit") }}</span>
-                <TooltipComponent :content="$t('message.deposit-tooltip')" />
-              </div>
-
-              <div
-                class="nls-font-500 text-dark-grey text-upper flex items-center items-center justify-end text-right text-12 md:flex"
-              >
-                {{ $t("message.yield") }}
-                <TooltipComponent :content="$t('message.earn-view-apr-tooltip')" />
-              </div>
-            </div>
-
-            <div
-              :class="{ 'animate-pulse': loading }"
-              class="block lg:mb-0"
-              role="status"
-            >
-              <template v-if="loading">
-                <div
-                  v-for="index in 2"
-                  :key="index"
-                  class="asset-partial nolus-box border-standart relative flex h-[67px] items-center items-center justify-between justify-between border-b px-4 py-3"
-                >
-                  <div class="w-[50%] grow-[1] md:w-auto">
-                    <div class="mb-2.5 h-1.5 w-32 rounded-full bg-grey"></div>
-                    <div class="h-1.5 w-24 rounded-full bg-grey"></div>
-                  </div>
-                  <div class="flex w-[50%] grow-[4] flex-col items-end md:w-auto md:items-start">
-                    <div class="mb-2.5 h-1.5 w-32 rounded-full bg-grey"></div>
-                    <div class="ml-8 h-1.5 w-24 rounded-full bg-grey"></div>
-                  </div>
-                  <div class="hidden h-1.5 w-12 rounded-full bg-grey md:flex"></div>
-                </div>
-              </template>
-              <template v-else>
-                <TransitionGroup
-                  appear
-                  name="fade"
-                  tag="div"
-                >
-                  <EarnLpnAsset
-                    v-for="(lpn, index) of lpnAsset"
-                    :key="lpn.key"
-                    :asset="lpn"
-                    :class="index > 0 ? 'border-t-[1px]' : ''"
-                    :cols="cols"
-                    :openSupplyWithdraw="() => openSupplyWithdrawDialog(lpn.key)"
-                  />
-
-                  <EarnNativeAsset
-                    key="nativeAsset"
-                    :asset="delegated"
-                    :cols="cols"
-                    :isDelegated="isDelegated"
-                    :openDelegateUndelegate="() => openDelegateUnDelegateDialog()"
-                  />
-                </TransitionGroup>
-              </template>
-            </div>
-          </div>
-        </div>
-        <!-- Portfolio -->
-
+        <!-- Assets -->
         <Table
           :class="['outline', { 'animate-pulse': loading }]"
           :columns="earningColumns"
           :title="$t('message.earning-assets')"
+          class="mt-6"
         >
           <template
             v-if="loading"
             v-slot:body
           >
-            <div
-              v-for="index in 2"
-              :key="index"
-              class="asset-partial nolus-box border-standart relative flex h-[67px] items-center items-center justify-between justify-between border-b px-4 py-3"
-            >
-              <div class="w-[50%] grow-[1] md:w-auto">
-                <div class="mb-2.5 h-1.5 w-32 rounded-full bg-grey"></div>
-                <div class="h-1.5 w-24 rounded-full bg-grey"></div>
-              </div>
-              <div class="flex w-[50%] grow-[4] flex-col items-end md:w-auto md:items-start">
-                <div class="mb-2.5 h-1.5 w-32 rounded-full bg-grey"></div>
-                <div class="ml-8 h-1.5 w-24 rounded-full bg-grey"></div>
-              </div>
-              <div class="hidden h-1.5 w-12 rounded-full bg-grey md:flex"></div>
-            </div>
+            <EarnAssetSkeleton />
           </template>
           <template
             v-else
@@ -129,10 +33,17 @@
               name="fade"
               tag="div"
             >
-              <EarnAssetRowWrapper
-                v-for="(asset, index) of assets"
-                :key="index"
-                :asset="asset"
+              <EarnLpnAsset
+                v-for="lpn of lpnAsset"
+                :key="lpn.key"
+                :asset="lpn"
+                :openSupplyWithdraw="() => openSupplyWithdrawDialog(lpn.key)"
+              />
+              <EarnNativeAsset
+                key="nativeAsset"
+                :asset="delegated"
+                :openDelegateUndelegate="() => openDelegateUnDelegateDialog()"
+                class="border-none"
               />
             </TransitionGroup>
           </template>
@@ -141,25 +52,18 @@
 
       <div class="lg:co-span-5 md:col-span-5">
         <!-- Rewards -->
-        <div
-          class="background border-standart shadow-box order-2 mt-6 block p-5 outline md:order-1 md:col-span-7 md:rounded-xl"
+        <Table
+          :title="$t('message.rewards')"
+          class="mt-6"
         >
-          <div class="border-standart flex items-center justify-between border-b pb-6">
-            <h2 class="nls-font-500 my-0 text-left text-16 text-primary">
-              {{ $t("message.rewards") }}
-            </h2>
-          </div>
-          <!-- Assets -->
-          <div class="block">
-            <!-- Assets Container -->
+          <template v-slot:body>
             <EarnReward
               :cols="cols"
               :onClickClaim="onClickWithdrawRewards"
               :reward="reward"
             />
-            <!-- Assets Container -->
-          </div>
-        </div>
+          </template>
+        </Table>
       </div>
     </div>
   </div>
@@ -211,9 +115,8 @@ import WithdrawRewardsDialog from "@/common/components/modals/WithdrawRewardsDia
 
 import Modal from "@/common/components/modals/templates/Modal.vue";
 import ErrorDialog from "@/common/components/modals/ErrorDialog.vue";
-import TooltipComponent from "@/common/components/TooltipComponent.vue";
 
-import { EarnLpnAsset, EarnNativeAsset, EarnReward } from "./components";
+import { EarnAssetSkeleton, EarnLpnAsset, EarnNativeAsset, EarnReward } from "./components";
 import { onMounted, onUnmounted, provide, ref, watch } from "vue";
 import { ChainConstants, NolusClient } from "@nolus/nolusjs";
 import { AppUtils, Logger, NetworkUtils, WalletManager } from "@/common/utils";
@@ -228,7 +131,6 @@ import { useI18n } from "vue-i18n";
 import { storeToRefs } from "pinia";
 import { useAdminStore } from "@/common/stores/admin";
 import { Table } from "web-components";
-import EarnAssetRowWrapper from "@/modules/earn/components/EarnAssetRowWrapper.vue";
 
 const i18n = useI18n();
 
@@ -254,7 +156,6 @@ const selectedAsset = ref("");
 const showErrorDialog = ref(false);
 const errorMessage = ref("");
 const loading = ref(true);
-const isDelegated = ref(false);
 const lpnAsset = ref<Asset[] | []>([]);
 const lpnReward = ref(new Dec(0));
 const application = useApplicationStore();
@@ -263,7 +164,7 @@ const admin = useAdminStore();
 const earningColumns = [
   { label: i18n.t("message.asset") },
   { label: i18n.t("message.deposit"), tooltip: i18n.t("message.deposit-tooltip"), class: "hidden md:flex" },
-  { label: i18n.t("message.yield"), tooltip: i18n.t("message.earn-view-apr-tooltip") }
+  { label: i18n.t("message.yield"), tooltip: i18n.t("message.earn-view-apr-tooltip"), class: "justify-end" }
 ];
 
 onMounted(async () => {
@@ -375,14 +276,7 @@ async function loadDelegated() {
     decimalDelegated = decimalDelegated.add(d);
   }
 
-  if (decimalDelegated.isPositive()) {
-    isDelegated.value = true;
-  }
-
-  console.info({ delegations, decimalDelegated });
-
   delegated.value = { balance: coin(decimalDelegated.truncate().toString(), NATIVE_ASSET.denom) };
-  console.info({ delegated: delegated.value });
 }
 
 async function loadLPNCurrency() {
@@ -442,8 +336,6 @@ async function loadLPNCurrency() {
   }
 
   lpnAsset.value = [...items, ...lpnCurrencies];
-
-  console.info({ lpnAsset: lpnAsset.value });
 }
 
 function openDelegateUnDelegateDialog() {
