e45f2de0b709a17e8c0e2a65c3a6a550fa02eab7
Teodor Dobrev <teodor.working@gmail.com>
2024-06-04 08:40:40 +0300

    move filter in own component

--- src/modules/lease/view.vue
+++ src/modules/lease/view.vue
@@ -1,98 +1,28 @@
 <template>
-  <div class="mb-sm-nolus-70 col-span-12">
-    <!-- Header -->
-    <div
-      class="mt-0 flex flex-col-reverse flex-wrap items-center justify-between lg:mt-0 lg:mt-4 lg:flex-row lg:px-0 lg:pt-[25px]"
-    >
-      <div class="left w-full md:w-1/2">
-        <div
-          v-if="leasesesData.length >= SORT_FEATURE_ENABLE"
-          class="sort disable background border-standart shadow-box mt-4 inline-flex h-full max-h-[44px] w-full items-center justify-between py-[12px] pl-[18px] pr-[24px] lg:mt-0 lg:w-auto lg:rounded-md"
-        >
-          <span class="icon icon-sort"> </span>
-
-          <button
-            class="flex"
-            @click="setSort(Sort.date)"
-          >
-            <span class="nls-font-500 ml-[21px] uppercase text-primary">{{ $t("message.date") }}</span>
-            <div class="ml-[2px] flex flex-col">
-              <span
-                class="icon icon-arrow-up-sort !text-[7px] text-[#8396B1]"
-                :class="{ '!text-[#2868E1]': SORT_TYPE.sort == Sort.date && SORT_TYPE.type == SortType.asc }"
-              >
-              </span>
-              <span
-                class="icon icon-arrow-down-sort !text-[7px] text-[#8396B1]"
-                :class="{ '!text-[#2868E1]': SORT_TYPE.sort == Sort.date && SORT_TYPE.type == SortType.desc }"
-              >
-              </span>
-            </div>
-          </button>
-
-          <button
-            class="flex"
-            @click="setSort(Sort.size)"
-          >
-            <span class="nls-font-500 ml-[21px] uppercase text-primary">{{ $t("message.size") }}</span>
-            <div class="ml-[2px] flex flex-col">
-              <span
-                class="icon icon-arrow-up-sort !text-[7px] text-[#8396B1]"
-                :class="{ '!text-[#2868E1]': SORT_TYPE.sort == Sort.size && SORT_TYPE.type == SortType.asc }"
-              >
-              </span>
-              <span
-                class="icon icon-arrow-down-sort !text-[7px] text-[#8396B1]"
-                :class="{ '!text-[#2868E1]': SORT_TYPE.sort == Sort.size && SORT_TYPE.type == SortType.desc }"
-              >
-              </span>
-            </div>
-          </button>
-
-          <button
-            class="flex"
-            @click="setSort(Sort.pnl)"
-          >
-            <span class="nls-font-500 ml-[21px] uppercase text-primary">{{ $t("message.pnl") }}</span>
-            <div class="ml-[2px] flex flex-col">
-              <span
-                class="icon icon-arrow-up-sort !text-[7px] text-[#8396B1]"
-                :class="{ '!text-[#2868E1]': SORT_TYPE.sort == Sort.pnl && SORT_TYPE.type == SortType.asc }"
-              >
-              </span>
-              <span
-                class="icon icon-arrow-down-sort !text-[7px] text-[#8396B1]"
-                :class="{ '!text-[#2868E1]': SORT_TYPE.sort == Sort.pnl && SORT_TYPE.type == SortType.desc }"
-              >
-              </span>
-            </div>
-          </button>
-        </div>
-        <template v-else>
-          <h1 class="nls-font-700 m-0 ml-[14px] mt-4 text-20 text-primary lg:ml-[0px] lg:mt-0">
-            {{ $t("message.leases") }}
-          </h1>
-        </template>
-      </div>
+  <div class="col-span-12">
+    <div class="mt-0 flex flex-col-reverse flex-wrap items-center justify-between lg:mt-10 lg:flex-row">
+      <LeaseFilter
+        :leases-data="leasesData"
+        :set-sort="setSort"
+        :sort-failure="SORT_FEATURE_ENABLE"
+        :sort-type="SORT_TYPE"
+      />
       <div class="right inline-flex w-full justify-start px-4 md:mt-0 md:w-1/2 md:justify-end lg:px-0">
-        <button
-          class="btn btn-primary btn-large-primary w-full md:w-auto"
+        <Button
+          :label="$t('message.lease-new')"
+          severity="primary"
+          size="large"
           @click="showLeaseModal = true"
-        >
-          {{ $t("message.lease-new") }}
-        </button>
+        />
       </div>
     </div>
-
-    <!-- Leases -->
-
     <TransitionGroup
-      name="fade-long"
       appear
+      name="fade-long"
       tag="div"
     >
       <div
-        v-for="lease in leasesesData"
+        v-for="lease in leasesData"
         :key="lease.leaseAddress"
       >
         <LeaseInfo :lease-info="lease" />
@@ -103,17 +33,17 @@
       >
         <div class="nls-12 text-dark-grey flex h-full flex-col items-center justify-center">
           <img
-            src="/src/assets/icons/empty_lease.svg"
             class="m-4 inline-block"
             height="32"
+            src="/src/assets/icons/empty_lease.svg"
             width="32"
           />
           <p class="text-center">
             {{ $t("message.empty-lease") }}
           </p>
           <a
-            @click="showLeaseModal = true"
             class="mt-2 cursor-pointer text-[#2868E1]"
+            @click="showLeaseModal = true"
           >
             {{ $t("message.lease-now") }}
           </a>
@@ -124,8 +54,8 @@
 
   <Modal
     v-if="showLeaseModal"
-    @close-modal="onCloseLease"
     route="create"
+    @close-modal="onCloseLease"
   >
     <LeaseDialog />
   </Modal>
@@ -134,8 +64,8 @@
     @close-modal="errorDialog.showDialog = false"
   >
     <ErrorDialog
-      :title="$t('message.error-connecting')"
       :message="errorDialog.errorMessage"
+      :title="$t('message.error-connecting')"
       :try-button="onTryAgain"
     />
   </Modal>
@@ -145,15 +75,15 @@
 import LeaseDialog from "@/common/components/modals/LeaseDialog.vue";
 import Modal from "@/common/components/modals/templates/Modal.vue";
 import ErrorDialog from "@/common/components/modals/ErrorDialog.vue";
+import { Button } from "web-components";
 
-import { LeaseInfo } from "./components";
-import { ref, provide, onMounted, onUnmounted, watch } from "vue";
+import { LeaseFilter, LeaseInfo } from "./components";
+import { computed, onMounted, onUnmounted, provide, ref, watch } from "vue";
 import { useLeases } from "@/common/composables";
 import { useApplicationStore } from "@/common/stores/application";
 import { storeToRefs } from "pinia";
 import { Sort, SortType } from "./types";
 import { Dec } from "@keplr-wallet/unit";
-import { computed } from "vue";
 
 const showLeaseModal = ref(false);
 const { leases, leaseLoaded, getLeases } = useLeases(onLeaseError);
@@ -161,6 +91,7 @@ const CHECK_TIME = 15000;
 const applicaton = useApplicationStore();
 const applicationRef = storeToRefs(applicaton);
 let timeOut: NodeJS.Timeout;
+
 const SORT_TYPE = ref({
   sort: Sort.date,
   type: SortType.desc
@@ -208,25 +139,7 @@ async function onTryAgain() {
   getLeases();
 }
 
-function setSort(sort: Sort) {
-  if (SORT_TYPE.value.sort == sort) {
-    switch (SORT_TYPE.value.type) {
-      case SortType.desc: {
-        SORT_TYPE.value.type = SortType.asc;
-        break;
-      }
-      case SortType.asc: {
-        SORT_TYPE.value.type = SortType.desc;
-        break;
-      }
-    }
-  } else {
-    SORT_TYPE.value.sort = sort;
-    SORT_TYPE.value.type = SortType.desc;
-  }
-}
-
-const leasesesData = computed(() => {
+const leasesData = computed(() => {
   const ls = leases.value;
 
   switch (SORT_TYPE.value.sort) {
@@ -267,11 +180,28 @@ const leasesesData = computed(() => {
         );
       }
     }
-    default: {
+    default:
       return ls;
-    }
   }
 });
+
+function setSort(sort: Sort) {
+  if (SORT_TYPE.value.sort == sort) {
+    switch (SORT_TYPE.value.type) {
+      case SortType.desc: {
+        SORT_TYPE.value.type = SortType.asc;
+        break;
+      }
+      case SortType.asc: {
+        SORT_TYPE.value.type = SortType.desc;
+        break;
+      }
+    }
+  } else {
+    SORT_TYPE.value.sort = sort;
+    SORT_TYPE.value.type = SortType.desc;
+  }
+}
 
 provide("getLeases", getLeases);
 </script>
