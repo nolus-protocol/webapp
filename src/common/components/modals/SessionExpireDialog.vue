b1bf64421b5bba42cd182f935dda337c62a3c759
Teodor Dobrev <teodor.working@gmail.com>
2024-07-17 14:02:29 +0300

    redesign SessionExpireDialog

--- src/common/components/modals/SessionExpireDialog.vue
+++ src/common/components/modals/SessionExpireDialog.vue
@@ -1,47 +1,40 @@
 <template>
   <div
-    class="background z-[101] mt-[52px] block h-full w-screen rounded-t-2xl border-border-color pb-[210px] pt-6 shadow-field-normal md:h-auto md:w-[516px] md:rounded-2xl md:border md:pb-10"
+    class="mx-auto mt-[62px] flex w-full max-w-[516px] flex-col rounded-xl bg-neutral-bg-50 px-10 py-6 text-center"
     @click.stop
   >
-    <div class="flex flex-col items-center">
-      <XMarkIcon class="radius-circle z-[5] h-14 w-14 bg-light-red/30 p-2 text-dark-red" />
-      <h1 class="z-[5] mt-4 text-center text-28 font-semibold text-neutral-typography-200 md:text-32">
+    <div class="flex flex-col items-center gap-6">
+      <XMarkIcon class="h-16 w-16 rounded-full bg-danger-50 p-2 text-danger-100" />
+      <h1
+        class="block w-full text-center text-28 font-semibold leading-7 text-neutral-typography-200 md:text-32 md:leading-8"
+      >
         {{ $t("message.session-timeout") }}
       </h1>
     </div>
 
     <div class="separator-line relative z-[5] py-6"></div>
 
-    <div class="flex flex-col items-center">
-      <p class="z-[5] px-4 text-center text-18 font-normal text-neutral-typography-200 md:px-10">
+    <div class="flex flex-col items-center gap-8">
+      <p
+        class="z-[5] w-full rounded-lg bg-dialogs-swap-color p-4 text-center text-14 font-normal text-neutral-typography-200"
+      >
         {{ $t("message.session-timeout-message") }}
       </p>
 
-      <div class="mt-6 hidden gap-2 md:flex">
-        <button
-          class="btn btn-primary btn-large-primary"
-          @click="close()"
-        >
-          {{ $t("message.refresh") }}
-        </button>
-      </div>
-
-      <div
-        class="align-center background inset-x-0 bottom-0 mx-auto flex justify-center p-4 text-center md:relative md:hidden md:pt-7"
-      >
-        <button
-          class="btn btn-primary btn-large-primary mr-4 w-40"
-          @click="close()"
-        >
-          {{ $t("message.refresh") }}
-        </button>
-      </div>
+      <Button
+        :label="$t('message.refresh')"
+        class="w-full"
+        severity="primary"
+        size="large"
+        @click="close()"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { XMarkIcon } from "@heroicons/vue/24/solid";
+import { Button } from "web-components";
 
 defineProps({
   close: {
