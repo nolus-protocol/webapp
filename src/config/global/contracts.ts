db1f2d0a536e2e8276905e83cc3d9ccbdcc01df6
Alexander Makedonski <alexanderm@prime-code.com>
2025-01-20 13:06:41 +0100

    ignore(OSMOSIS-OSMOSIS-ATOM)

--- src/config/global/contracts.ts
+++ src/config/global/contracts.ts
@@ -143,7 +143,7 @@ export const CONTRACTS: ContractConfig = {
     admin: {
       instance: "nolus1gurgpv8savnfw66lckwzn4zk7fp394lpe667dhu7aw48u40lj6jsqxf8nd",
       codeId: "",
-      ignoreProtocols: []
+      ignoreProtocols: ["OSMOSIS-OSMOSIS-ATOM"]
     }
   }
 };
