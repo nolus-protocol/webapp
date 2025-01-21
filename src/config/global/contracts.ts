595cdb59e0bb52fd360aa137bcd79cc22d64edfe
kostovster <kostovster@gmail.com>
2025-01-21 10:54:17 +0200

    update: ignoreProtocols

--- src/config/global/contracts.ts
+++ src/config/global/contracts.ts
@@ -154,7 +154,7 @@ export const CONTRACTS: ContractConfig = {
     admin: {
       instance: "nolus1gurgpv8savnfw66lckwzn4zk7fp394lpe667dhu7aw48u40lj6jsqxf8nd",
       codeId: "",
-      ignoreProtocols: [ignoreProtocols: "OSMOSIS-OSMOSIS-ATOM", "OSMOSIS-OSMOSIS-OSMO, OSMOSIS-OSMOSIS-INJ"]
+      ignoreProtocols: [ignoreProtocols: "OSMOSIS-OSMOSIS-ATOM", "OSMOSIS-OSMOSIS-OSMO", "OSMOSIS-OSMOSIS-INJ"]
     }
   }
 };
