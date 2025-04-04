a393d9a22a8d556f3ed5a9a056596bc426e815b6
alexandermake <alexanderm@prime-code.com>
2025-04-04 08:56:05 +0300

    update(due projection secs)

--- src/config/global/due-projection-secs-url.ts
+++ src/config/global/due-projection-secs-url.ts
@@ -3,7 +3,7 @@ import { isDev } from "./modes";
 let url: string | Promise<string> = import("../lease/due-projection-secs?url").then((t) => t.default);
 
 if (!isDev()) {
-  url = "https://raw.githubusercontent.com/nolus-protocol/webapp/main/src/config/lease/due-projection-secs";
+  url = "https://raw.githubusercontent.com/nolus-protocol/webapp/main/src/config/lease/due-projection-secs.json";
 }
 
 export const DUE_PROJECTION_SECS_URL = url;
